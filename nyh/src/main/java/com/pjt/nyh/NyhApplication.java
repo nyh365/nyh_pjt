0a198b208dfc9e199e82974a9bc3898376fffb52
나유현 <12nyk34@naver.com>
2023-08-27 02:43:25 +0900

    init

---
+++ nyh/src/main/java/com/pjt/nyh/NyhApplication.java
@@ -0,0 +1,13 @@
+package com.pjt.nyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyhApplication.class, args);
+	}
+
+}
---
+++ nyh/src/test/java/com/pjt/nyh/NyhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjt.nyh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
